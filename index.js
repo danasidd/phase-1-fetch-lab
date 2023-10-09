0d237cfaf5bdfa87241ba5db45094377d3e9240f
Dana AlSiddig <danlsiddig@gmail.com>
2023-10-09 15:59:31 +0300

    Done

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Fetch data from the API and return the promise
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then((response) => response.json())
+    .then((data) => renderBooks(data)) // Call renderBooks with the fetched data
+    .catch((error) => console.error('Error fetching books:', error)); // Handle errors
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach((book) => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+  // Call fetchBooks() when the DOM is loaded
   fetchBooks();
 });
